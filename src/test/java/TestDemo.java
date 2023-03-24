0901fddf08d31b46d7f4d8f497b1fc1c7de1c05a
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-24 11:09:27 +0800

    feat: init

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDemo {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
