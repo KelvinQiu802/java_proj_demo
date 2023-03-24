575b0d32a9a2adbf6aaf20bbca6bdbd7b7e8a9ef
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-24 11:22:29 +0800

    feat: Commit test

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestDemo {
     @Test
     public void test() {
-        System.out.println("TEST");
         assertTrue(true);
     }
 }
